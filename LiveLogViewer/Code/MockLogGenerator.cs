bb1c6ea285e6b2e13bdc33c870a18903ab1abbd2
Adrian Padilla Vega <adrianpadilla@live.com>
2013-10-31 21:49:00 -0700

    Modified Mock for dynamically generating new entries.

--- LiveLogViewer/Code/MockLogGenerator.cs
+++ LiveLogViewer/Code/MockLogGenerator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using LiveLogViewer.Models;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LiveLogViewer
 {
@@ -11,21 +13,50 @@ namespace LiveLogViewer
     /// </summary>
     public static class MockLogGenerator
     {
+        static List<LogEntry> List = new List<LogEntry>();
+
+        static Random Random = new Random();
+
+        static MockLogGenerator()
+        {
+                List.Add(new LogEntry()
+                {
+                    Activity = 10000,
+                    EntryDateTime = DateTime.Now.AddSeconds(-3),
+                    Message = string.Format("Mock Message: {0}", Guid.NewGuid().ToString())
+                });
+
+                Task.Factory.StartNew(() =>
+                {
+                    GenerateLogLoop();
+                });
+
+        }
+
+        private static void GenerateLogLoop()
+        {
+            int next = 0;
+            while (true)
+            {
+                List.Add(new LogEntry()
+                {
+                    Activity = 10000,
+                    EntryDateTime = DateTime.Now,
+                    Message = string.Format("Mock Message: {0}", Guid.NewGuid().ToString())
+                });
+
+                //Removes older entries
+                List.RemoveAll(l => l.EntryDateTime < DateTime.Now.AddSeconds(-10));
+
+                next = Random.Next(1000);
+
+                Thread.Sleep(next);
+            }
+        }
+        
         public static List<LogEntry> GetStaticExampleLogs()
         {
-            List<LogEntry> list = new List<LogEntry>();
-
-            for (int i = 0; i < 50; i++)
-            {
-                list.Add(new LogEntry()
-                {
-                     Activity = 10000,
-                     EntryDateTime = DateTime.Now.AddHours(-1),
-                     Message = string.Format("Mock Message #{0:d}: {1}", i, Guid.NewGuid().ToString())
-                });
-            }
-
-            return list;
+            return List;
         }
     }
 }
